e3f2e122d6ef8e76110144aff32e21de2283ac2c
Augusto Cesar <guto_slv@live.com>
2018-06-09 12:13:56 -0300

    Creates the tasks route

--- bootcamp-api/index.js
+++ bootcamp-api/index.js
@@ -7,6 +7,16 @@ app.get('/', (req, res) => {
   res.json({ status: 'Bootcamp API OK!'});
 });
 
+app.get('/tasks', (req, res) => {
+  res.json({
+    tasks: [
+      { name: 'Pão' },
+      { name: 'Nanana' },
+      { name: 'Pão-Nanana'}
+    ]
+  });
+});
+
 app. listen (port, () => {
   console.log(`Bootcamp API - porta ${port}`);
-})
+});
