61ede3cafd19c6674274bd4fd30a8e06c2217808
Augusto Cesar <guto_slv@live.com>
2018-06-09 11:38:24 -0300

    Shows another import declaration

---
+++ exercicio3/operations.js
@@ -0,0 +1,2 @@
+exports.sum = (value1, value2) => value1 + value2;
+exports.subtraction = (value1, value2) => value2 - value1;
---
+++ exercicio3/server.js
@@ -0,0 +1,4 @@
+const { sum, subtraction } = require('./operations');
+
+console.log(sum(1,2));
+console.log(subtraction(2,1));
