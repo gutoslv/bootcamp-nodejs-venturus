e75be33221ddb43819eedb65744a410f330cedfd
Augusto Cesar <guto_slv@live.com>
2018-06-09 12:07:27 -0300

    Creates the index.js listening on port 3000

---
+++ bootcamp-api/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const port = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ status: 'Bootcamp API OK!'});
+});
+
+app. lister (port, () => {
+  console.log(`Bootcamp API - porta ${port}`);
+})
