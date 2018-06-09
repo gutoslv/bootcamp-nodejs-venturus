b7467bd7c9c24b562f4eaae4f2d2d082883d0dc5
Augusto Cesar <guto_slv@live.com>
2018-06-09 12:07:41 -0300

    Mistype error

--- bootcamp-api/index.js
+++ bootcamp-api/index.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
   res.json({ status: 'Bootcamp API OK!'});
 });
 
-app. lister (port, () => {
+app. listen (port, () => {
   console.log(`Bootcamp API - porta ${port}`);
 })
